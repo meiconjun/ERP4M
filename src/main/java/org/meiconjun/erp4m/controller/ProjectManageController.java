88b23c2debac1a8b5712ee4b620f6c33e487dc24
meiconjun <baoshiki@vip.qq.com>
2020-05-25 17:33:43 +0800

    日常更新

---
+++ src/main/java/org/meiconjun/erp4m/controller/ProjectManageController.java
@@ -0,0 +1,23 @@
+package org.meiconjun.erp4m.controller;
+
+import org.meiconjun.erp4m.base.BaseController;
+import org.meiconjun.erp4m.bean.RequestBean;
+import org.meiconjun.erp4m.bean.ResponseBean;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Lch
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2020/5/25 17:26
+ */
+@Controller
+public class ProjectManageController extends BaseController {
+    @Override
+    protected ResponseBean doAction(HttpServletRequest request, RequestBean requestBean) {
+        return null;
+    }
+}
